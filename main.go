a532d1af92c624551790d2130e153bbd58edb15c
Pedro Mendes <pedro.mendes@peixeurbano.com>
2018-05-19 17:00:50 -0300

    main: add basic flow to dequeue messages from SQS

--- main.go
+++ main.go
@@ -1,6 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"errors"
+	log "github.com/sirupsen/logrus"
+	"github.com/pedrolopesme/iss-notifier/sqs"
+)
+
+const (
+	// URL to our queue
+	SQSEnvVar = "ISS_SQS_URL"
+)
+
+var (
+	// ErrSQSVarNotFound is returned when a ENV Var with ISS SQS URL wasn't found
+	ErrSQSVarNotFound = errors.New("environment variable with ISS SQS URL not set. Please define a env var " + SQSEnvVar)
+)
 
 // ISS Position relative to Earth
 type Coordinate struct {
@@ -9,6 +25,16 @@ type Coordinate struct {
 }
 
 func main(){
+
+	// Retrieving ISS_SQS_URL from the ENV vars
+	queueURL := os.Getenv(SQSEnvVar)
+	if queueURL == "" {
+		err := ErrSQSVarNotFound
+		log.Error(err)
+		return
+	}
+
 	fmt.Print("Checking out SQS Queue")
+	sqs.Consume(queueURL)
 }
 
