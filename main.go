17f5eaa46bc35cfd8b2cd794204eadb3ef6e9380
Pedro Mendes <pedro.mendes@peixeurbano.com>
2018-05-19 16:11:36 -0300

    main: add main.go with Coordinate struct

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// ISS Position relative to Earth
+type Coordinate struct {
+	Latitude float64
+	Longitude float64
+}
+
+func main(){
+	fmt.Print("Checking out SQS Queue")
+}
+
