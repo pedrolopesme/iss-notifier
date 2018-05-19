7a55f22457bd9322d8dcb38020797d12d8dd4076
Pedro Mendes <pedro.mendes@peixeurbano.com>
2018-05-19 17:00:10 -0300

    sqs/consumer: retrieving messages from SQS

---
+++ sqs/consumer.go
@@ -0,0 +1,48 @@
+package sqs
+
+import (
+	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"fmt"
+	"os"
+)
+
+// Connects to ISS SQS Queue and returns al coordinates stored there.
+func Consume(queueUrl string) (message string, err error) {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	svc := sqs.New(sess)
+
+	result, err := svc.ReceiveMessage(&sqs.ReceiveMessageInput{
+		QueueUrl: &queueUrl,
+		AttributeNames: aws.StringSlice([]string{
+			"SentTimestamp",
+		}),
+		MaxNumberOfMessages: aws.Int64(1),
+		MessageAttributeNames: aws.StringSlice([]string{
+			"All",
+		}),
+		WaitTimeSeconds: aws.Int64(20),
+	})
+
+	if err != nil {
+		exitErrorf("Unable to receive message from queue %q, %v.", queueUrl, err)
+	}
+
+	fmt.Printf("Received %d messages.\n", len(result.Messages))
+	if len(result.Messages) > 0 {
+		for _, message := range result.Messages {
+			fmt.Println(message)
+		}
+	}
+
+	return
+}
+
+func exitErrorf(msg string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, msg+"\n", args...)
+	os.Exit(1)
+}
